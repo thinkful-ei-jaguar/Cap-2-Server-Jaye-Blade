df7bb4b3d0e29f32bca8e178435cd3b6aba9c031
Jaye Anne Laguardia <jayelaguardia@gmail.com>
2020-03-18 04:53:40 -0700

    save test deploy

--- src/language/language-router.js
+++ src/language/language-router.js
@@ -71,50 +71,50 @@ languageRouter
       })
     }
     
-    let list = new Link()
-    const orgwords = await LanguageService.populate(req.app.get('db'), req.language.id)
-    orgwords.rows.map(word => list.insertLast(word.original))
+    // let list = new Link()
+    // const orgwords = await LanguageService.populate(req.app.get('db'), req.language.id)
+    // orgwords.rows.map(word => list.insertLast(word.original))
     
-    let answer = await LanguageService.getWord(req.app.get('db'), req.language.id, list.head.value)
-    answer = answer.rows[0].translation.trim()
-    const values = await LanguageService.getValues(req.app.get('db'), req.language.id, list.head.value)
-    let { memory_value, correct_count, incorrect_count, total_score } = values.rows[0]
-    let isCorrect
-    if(guess === answer){
-      isCorrect = true
-      memory_value *= 2
-      correct_count += 1
-      total_score += 1
-    }
-    else {
-      isCorrect = false
-      memory_value = 1
-      incorrect_count += 1
-    }
-    const newValues = {
-      memory_value, 
-      correct_count, 
-      incorrect_count, 
-      total_score
-    }
-    LanguageService.setValues(req.app.get('db'), req.language.id, list.head.value, newValues)
-    temp = list.head.value
-    list.remove(list.head.value)
-    list.insertAt(temp, memory_value)
-    const nextValues = await LanguageService.getValues(req.app.get('db'), req.language.id, list.head.value)
-    correct_count = nextValues.rows[0].correct_count
-    incorrect_count = nextValues.rows[0].incorrect_count
-    
+    // let answer = await LanguageService.getWord(req.app.get('db'), req.language.id, list.head.value)
+    // answer = answer.rows[0].translation.trim()
+    // const values = await LanguageService.getValues(req.app.get('db'), req.language.id, list.head.value)
+    // let { memory_value, correct_count, incorrect_count, total_score } = values.rows[0]
+    // let isCorrect
+    // if(guess === answer){
+    //   isCorrect = true
+    //   memory_value *= 2
+    //   correct_count += 1
+    //   total_score += 1
+    // }
+    // else {
+    //   isCorrect = false
+    //   memory_value = 1
+    //   incorrect_count += 1
+    // }
+    // const newValues = {
+    //   memory_value, 
+    //   correct_count, 
+    //   incorrect_count, 
+    //   total_score
+    // }
+    // LanguageService.setValues(req.app.get('db'), req.language.id, list.head.value, newValues)
+    // temp = list.head.value
+    // list.remove(list.head.value)
+    // list.insertAt(temp, memory_value)
+    // const nextValues = await LanguageService.getValues(req.app.get('db'), req.language.id, list.head.value)
+    // correct_count = nextValues.rows[0].correct_count
+    // incorrect_count = nextValues.rows[0].incorrect_count
 
-    const output = {
-      nextWord: list.head.value,
-      wordCorrectCount: correct_count,
-      wordIncorrectCount: incorrect_count,
-      totalScore: total_score,
-      answer,
-      isCorrect,
-    }
-    res.json(output)
+
+    // const output = {
+    //   nextWord: list.head.value,
+    //   wordCorrectCount: correct_count,
+    //   wordIncorrectCount: incorrect_count,
+    //   totalScore: total_score,
+    //   answer,
+    //   isCorrect,
+    // }
+    // res.json(output)
   })
 
 module.exports = languageRouter;
